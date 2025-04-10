40ce419811cc161cd85daabe3aa426db508ef7f9
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-04-10 03:44:01 +0000

    test more of the 7_ script piecemeal

--- aws_EC2_boto3_class/sequential_master/7_test.py
+++ aws_EC2_boto3_class/sequential_master/7_test.py
@@ -16,3 +16,27 @@ load_dotenv()
 
 # Confirm script execution
 print("Script started")
+
+
+# Set variables from environment
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+region_name = os.getenv("region_name")
+
+# Check for missing environment variables
+if not aws_access_key or not aws_secret_key or not region_name:
+    logger.error("Missing AWS credentials or region name in environment variables.")
+    raise ValueError("Missing AWS credentials or region name in environment variables.")
+
+# Establish a session with AWS
+
+logger.info("Establishing a session with AWS...")
+
+session = boto3.Session(
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name=region_name
+)
+
+
+logger.info("Session established successfully.")
