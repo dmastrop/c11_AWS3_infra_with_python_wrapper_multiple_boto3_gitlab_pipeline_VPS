7bc11838563372733cbffa267e24181a8f95ba27
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-04-10 21:11:38 +0000

    remove logging on this and try to pull env vars on 7_

---
+++ aws_EC2_boto3_class/sequential_master/7_debug26.py
@@ -0,0 +1,21 @@
+
+import os
+
+# Confirm script execution
+print("Script started")  # This should print
+
+# Print environment variables
+print("Environment Variables:")
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
+
+# Set variables from environment
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+region_name = os.getenv("region_name")
+
+# Print out specific environment variables for debugging
+print(f"AWS_ACCESS_KEY_ID: {aws_access_key}")
+print(f"AWS_SECRET_ACCESS_KEY: {aws_secret_key}")
+print(f"region_name: {region_name}")
+
