ec15b0685db2ea45aa70273efc753b004bef5296
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-04-30 00:18:41 +0000

    multi-threading failing to run parallel becasue of the exec function expecting string or full path to the .py modules rather than object names of the imported modules.  Also added loging for the run_module function. This would not normally happen if we were calling functions in the modules. Then the  module object would have worked and full path not required

--- aws_boto3_modular_multi_threading/master_script.py
+++ aws_boto3_modular_multi_threading/master_script.py
@@ -1,4 +1,8 @@
 import threading
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
+
 from sequential_master_modules import Elastic_Beanstalk_ORIGINAL_with_environment_id_WORKING_VERSION_BY_ElasticLB_env_id_without_RDS
 from sequential_master_modules import RDS_and_security_group_json
 from sequential_master_modules import jumphost_for_RDS_mysql_client_NEW3
@@ -11,30 +15,51 @@ from sequential_master_modules import HTTPS_wget_for_elastic_beanstalk_ALB
 # the exec function below will get the entire block of code as a string
 # The `exec` function executes the string of code read from the file. This means the entire script is executed within the context of the `run_module` function.
 
-def run_module(module_script):
-    print(f"Starting module script: {module_script}")
-    exec(open(module_script).read())
-    print(f"Completed module script: {module_script}")
+# NOTE: The `exec` function expects a string, bytes, or os.PathLike object, not a module object. Cannot simply
+# pass a module object but need full path to the module script. Since this is docker container it has to be
+# path defined by HOME DIRECTORY in the container not the pipeline source code.
+# define new variable module_script_path instead of module_script object from the imports above
+
+def run_module(module_script_path):
+    logging.debug(f"Starting module script: {module_script_path}")
+    exec(open(module_script_path).read())
+    logging.debug(f"Completed module script: {module_script_path}")
 
 
 def main():
     # Create and start threads for the first two modules
-    thread1 = threading.Thread(target=run_module, args=(Elastic_Beanstalk_ORIGINAL_with_environment_id_WORKING_VERSION_BY_ElasticLB_env_id_without_RDS,))
-    thread2 = threading.Thread(target=run_module, args=(RDS_and_security_group_json,))
+    #thread1 = threading.Thread(target=run_module, args=(Elastic_Beanstalk_ORIGINAL_with_environment_id_WORKING_VERSION_BY_ElasticLB_env_id_without_RDS,))
+    #thread2 = threading.Thread(target=run_module, args=(RDS_and_security_group_json,))
+    #thread1.start()
+    #thread2.start()
+
+    thread1 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/Elastic_Beanstalk_ORIGINAL_with_environment_id_WORKING_VERSION_BY_ElasticLB_env_id_without_RDS.py",))
+    thread2 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/RDS_and_security_group_json.py",))
     thread1.start()
     thread2.start()
+
 
     # Wait for the first two threads to complete. main() will be in blocking until both of the threads are complete which is why we need to have both thread1.join and thread2.join. Both have to be completed prior to moving onto the next group of threads below.
     thread1.join()
     thread2.join()
 
     # Create and start threads for the last three modules
-    thread3 = threading.Thread(target=run_module, args=(jumphost_for_RDS_mysql_client_NEW3,))
-    thread4 = threading.Thread(target=run_module, args=(wget_for_elastic_beanstalk_ALB,))
-    thread5 = threading.Thread(target=run_module, args=(HTTPS_wget_for_elastic_beanstalk_ALB,))
+    #thread3 = threading.Thread(target=run_module, args=(jumphost_for_RDS_mysql_client_NEW3,))
+    #thread4 = threading.Thread(target=run_module, args=(wget_for_elastic_beanstalk_ALB,))
+    #thread5 = threading.Thread(target=run_module, args=(HTTPS_wget_for_elastic_beanstalk_ALB,))
+    #thread3.start()
+    #thread4.start()
+    #thread5.start()
+
+
+    thread3 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/jumphost_for_RDS_mysql_client_NEW3.py",))
+    thread4 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/wget_for_elastic_beanstalk_ALB.py",))
+    thread5 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/HTTPS_wget_for_elastic_beanstalk_ALB.py",))
     thread3.start()
     thread4.start()
     thread5.start()
+
+
 
     # Wait for the last three threads to complete. main() will be blocking until all threads are complete. See above.
     thread3.join()
