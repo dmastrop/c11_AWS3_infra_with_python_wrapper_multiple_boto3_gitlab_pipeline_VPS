8ddc72d4282c04417f9aac878607aed29852e03b
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-05-06 00:42:51 +0000

    added thread-id logging to the thread creates so that logs are clearer in the gitlab pipeline console

--- aws_boto3_modular_multi_threading/master_script_NO_thread_id_logging.py
+++ aws_boto3_modular_multi_threading/master_script_NO_thread_id_logging.py
@@ -40,17 +40,8 @@ def main():
     #thread1.start()
     #thread2.start()
 
-
-    # ORIGINAL thread definitions wtihout thread-id logging
-    #thread1 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/Elastic_Beanstalk_ORIGINAL_with_environment_id_WORKING_VERSION_BY_ElasticLB_env_id_without_RDS.py",))
-    #thread2 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/RDS_and_security_group_json.py",))
-    
-    # NEW thread definitions with thread-id logging
-    thread1 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/Elastic_Beanstalk_ORIGINAL_with_environment_id_WORKING_VERSION_BY_ElasticLB_env_id_without_RDS.py",), name='Thread-1')
-    thread2 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/RDS_and_security_group_json.py",), name='Thread-2')
-
-
-
+    thread1 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/Elastic_Beanstalk_ORIGINAL_with_environment_id_WORKING_VERSION_BY_ElasticLB_env_id_without_RDS.py",))
+    thread2 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/RDS_and_security_group_json.py",))
     thread1.start()
     thread2.start()
 
@@ -58,11 +49,6 @@ def main():
     # Wait for the first two threads to complete. main() will be in blocking until both of the threads are complete which is why we need to have both thread1.join and thread2.join. Both have to be completed prior to moving onto the next group of threads below.
     thread1.join()
     thread2.join()
-
-
-
-
-
 
     # Create and start threads for the last three modules
     #thread3 = threading.Thread(target=run_module, args=(jumphost_for_RDS_mysql_client_NEW3,))
@@ -72,21 +58,10 @@ def main():
     #thread4.start()
     #thread5.start()
 
-    # ORIGINAL thread definitions wtihout thread-id logging
-    #thread3 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/jumphost_for_RDS_mysql_client_NEW3.py",))
-    #thread4 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/wget_for_elastic_beanstalk_ALB.py",))
-    #thread5 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/HTTPS_wget_for_elastic_beanstalk_ALB.py",))
-    
 
-
-
-
-    # NEW thread definitions with thread-id logging
-    thread3 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/jumphost_for_RDS_mysql_client_NEW3.py",), name='Thread-3')
-    thread4 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/wget_for_elastic_beanstalk_ALB.py",), name='Thread-4')
-    thread5 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/HTTPS_wget_for_elastic_beanstalk_ALB.py",), name='Thread-5')
-
-
+    thread3 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/jumphost_for_RDS_mysql_client_NEW3.py",))
+    thread4 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/wget_for_elastic_beanstalk_ALB.py",))
+    thread5 = threading.Thread(target=run_module, args=("/aws_EC2/sequential_master_modules/HTTPS_wget_for_elastic_beanstalk_ALB.py",))
     thread3.start()
     thread4.start()
     thread5.start()
