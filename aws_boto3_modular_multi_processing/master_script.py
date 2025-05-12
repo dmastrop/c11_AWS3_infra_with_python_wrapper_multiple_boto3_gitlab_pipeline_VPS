90a7cdd6d24f6a168eb5259cfc22361a69740913
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-05-12 22:50:41 +0000

    rename moudle2 wiht 8 and 12 combination

--- aws_boto3_modular_multi_processing/master_script.py
+++ aws_boto3_modular_multi_processing/master_script.py
@@ -19,13 +19,20 @@ def restart_ec_multiple_instances():
     run_module("/aws_EC2/sequential_master_modules/restart_the_EC_multiple_instances_with_client_method_DEBUG.py")
 
 
-# this one will run with the mutli-processing chnanges to the module2
-#def install_tomcat_on_instances():
-#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3.py")
+# this one will run with the mutli-processing chnanges to the module2 with 6 processes and 6 threads per process
+def install_tomcat_on_instances():
+    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3.py")
 
 # this one will run without the multi-processing changes to the module2
-def install_tomcat_on_instances():
-    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_ORIGINAL_without_main.py")
+#def install_tomcat_on_instances():
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_ORIGINAL_without_main.py")
+
+# this one will run wiht multi-processing chnages to the module2 with 8 processes and 12 threads per process
+#def install_tomcat_on_instances():
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_8_and_12.py")
+
+
+
 
 
 def save_instance_ids_and_security_group_ids():
