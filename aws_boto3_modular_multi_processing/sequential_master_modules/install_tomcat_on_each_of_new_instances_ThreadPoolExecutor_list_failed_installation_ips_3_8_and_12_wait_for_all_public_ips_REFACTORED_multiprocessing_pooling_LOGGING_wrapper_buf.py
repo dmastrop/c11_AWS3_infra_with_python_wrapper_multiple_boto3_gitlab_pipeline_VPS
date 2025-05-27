fa8d3bf25a703fd14ed55bec34306f3a6550cb5d
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-05-27 02:45:26 +0000

    the exponential backoff is working very well. 45 desired count completed in about 16 minutes. INcrease desired_count to 70

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_8_and_12_wait_for_all_public_ips_REFACTORED_multiprocessing_pooling_LOGGING_wrapper_buf.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_8_and_12_wait_for_all_public_ips_REFACTORED_multiprocessing_pooling_LOGGING_wrapper_buf.py
@@ -1087,7 +1087,7 @@ def main():
     ### Configurable parameters
     chunk_size = 2     # Number of IPs per process
     max_workers = 2       # Threads per process
-    desired_count = 45     # Max concurrent processes
+    desired_count = 70     # Max concurrent processes
 
     chunks = [instance_ips[i:i + chunk_size] for i in range(0, len(instance_ips), chunk_size)]
 
