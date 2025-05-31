96b921650767ac71539b6113aeb01881157f74e3
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-05-31 03:15:33 +0000

    change desired count to 100 with 150 processes

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_8_and_12_wait_for_all_public_ips_REFACTORED_multiprocessing_pooling_LOGGING_wrapper_buf_MAIN_LOGGING.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_8_and_12_wait_for_all_public_ips_REFACTORED_multiprocessing_pooling_LOGGING_wrapper_buf_MAIN_LOGGING.py
@@ -1192,7 +1192,7 @@ def main():
     ### Configurable parameters
     chunk_size = 2     # Number of IPs per process
     max_workers = 2       # Threads per process
-    desired_count = 75     # Max concurrent processes
+    desired_count = 100     # Max concurrent processes
 
     chunks = [instance_ips[i:i + chunk_size] for i in range(0, len(instance_ips), chunk_size)]
 
