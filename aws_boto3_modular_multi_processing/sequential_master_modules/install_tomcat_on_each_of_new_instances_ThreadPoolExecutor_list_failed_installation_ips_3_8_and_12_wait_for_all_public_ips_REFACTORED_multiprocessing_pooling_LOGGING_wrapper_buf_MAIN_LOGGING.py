5eeca65689790bb14ef98e769d4ef85948490604
Ubuntu <ubuntu@ip-172-31-81-160.ec2.internal>
2025-06-26 00:49:40 +0000

    test with 450 and 25 pooled, desired count of 425

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_8_and_12_wait_for_all_public_ips_REFACTORED_multiprocessing_pooling_LOGGING_wrapper_buf_MAIN_LOGGING.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_ThreadPoolExecutor_list_failed_installation_ips_3_8_and_12_wait_for_all_public_ips_REFACTORED_multiprocessing_pooling_LOGGING_wrapper_buf_MAIN_LOGGING.py
@@ -1295,7 +1295,7 @@ def main():
     ### Configurable parameters
     chunk_size = 1     # Number of IPs per process
     max_workers = 1       # Threads per process
-    desired_count = 375   # Max concurrent processes
+    desired_count = 425   # Max concurrent processes
 
     chunks = [instance_ips[i:i + chunk_size] for i in range(0, len(instance_ips), chunk_size)]
 
