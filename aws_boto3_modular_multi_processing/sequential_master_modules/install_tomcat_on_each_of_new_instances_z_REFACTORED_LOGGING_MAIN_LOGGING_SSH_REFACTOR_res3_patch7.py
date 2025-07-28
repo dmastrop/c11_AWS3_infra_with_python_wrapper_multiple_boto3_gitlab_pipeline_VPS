82b5a820161fc39bfd518e09fbe6d1f4eb2869db
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-07-28 23:17:54 +0000

    desired_count of 12 with 12 instances to run quick test for logging issue with resurrection_monitor

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_z_REFACTORED_LOGGING_MAIN_LOGGING_SSH_REFACTOR_res3_patch7.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_z_REFACTORED_LOGGING_MAIN_LOGGING_SSH_REFACTOR_res3_patch7.py
@@ -2142,7 +2142,7 @@ def main():
     ### Configurable parameters
     chunk_size = 1     # Number of IPs per process
     max_workers = 1       # Threads per process
-    desired_count = 487   # Max concurrent processes
+    desired_count = 12   # Max concurrent processes
 
     chunks = [instance_ips[i:i + chunk_size] for i in range(0, len(instance_ips), chunk_size)]
 
