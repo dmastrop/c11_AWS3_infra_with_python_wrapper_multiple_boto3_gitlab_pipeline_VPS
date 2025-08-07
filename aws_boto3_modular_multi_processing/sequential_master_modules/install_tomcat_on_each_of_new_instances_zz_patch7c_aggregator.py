74c6cf0e2ccb3b4f8321f7c4c7f5b61cd33a2f38
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-08-07 05:52:02 +0000

    test with desired count of 6 for 6 processes 12 nodes and 2 threads per process and see if run_test giving correct registry

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_aggregator.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_aggregator.py
@@ -3266,9 +3266,9 @@ def main():
     ]
 
     ### Configurable parameters
-    chunk_size = 1     # Number of IPs per process
-    max_workers = 1       # Threads per process
-    desired_count = 12   # Max concurrent processes
+    chunk_size = 2     # Number of IPs per process
+    max_workers = 2       # Threads per process
+    desired_count = 6   # Max concurrent processes
 
     chunks = [instance_ips[i:i + chunk_size] for i in range(0, len(instance_ips), chunk_size)]
 
