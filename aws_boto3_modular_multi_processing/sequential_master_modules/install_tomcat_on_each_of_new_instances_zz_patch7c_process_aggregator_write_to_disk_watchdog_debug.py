f4c26d2daf18fa8e6a1e06a65a527db2a6496cfa
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-08-14 01:08:11 +0000

    try 32 node count

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug.py
@@ -3742,7 +3742,7 @@ def main():
     ### Configurable parameters
     chunk_size = 1    # Number of IPs per process
     max_workers = 1       # Threads per process
-    desired_count = 512   # Max concurrent processes
+    desired_count = 32   # Max concurrent processes
 
     chunks = [instance_ips[i:i + chunk_size] for i in range(0, len(instance_ips), chunk_size)]
 
