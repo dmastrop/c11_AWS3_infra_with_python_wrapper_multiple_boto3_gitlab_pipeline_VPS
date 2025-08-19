c71c73ccc8a67864aadca13a1bf2e78c6c095f3a
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-08-19 01:54:01 +0000

    switch back to current patch7d code. need to debug problem with benchmark_ips. The older code did not have the problem. The problem is in res monitor function

--- aws_boto3_modular_multi_processing/master_script.py
+++ aws_boto3_modular_multi_processing/master_script.py
@@ -175,15 +175,15 @@ def restart_ec_multiple_instances():
 #    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug.py")
 
 
-#### THIS IS THE MODULE2 SCRIPT with adaptive watchdog timeout code added to write to disk and patch7d res monitor code
-def install_tomcat_on_instances():
-    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d.py")
-
-
-##### THIS IS THE MODULE2 SCRIPT with res monitor patch7d changes to tomcat_worker and threaded_install for instance_info
+##### THIS IS THE MODULE2 SCRIPT with adaptive watchdog timeout code added to write to disk and patch7d res monitor code
 #def install_tomcat_on_instances():
-#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d_1.py")
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d.py")
 #
+
+#### THIS IS THE MODULE2 SCRIPT with res monitor patch7d changes to tomcat_worker and threaded_install for instance_info
+def install_tomcat_on_instances():
+    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d_1.py")
+
 
 
 
