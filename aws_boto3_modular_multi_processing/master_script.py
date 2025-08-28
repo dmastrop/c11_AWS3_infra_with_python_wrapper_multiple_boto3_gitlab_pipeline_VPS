6dc06c3d1b3fc766c4f5b0a6ec0a425c77157778
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-08-28 04:27:33 +0000

    issues with the last commit, back out

--- aws_boto3_modular_multi_processing/master_script.py
+++ aws_boto3_modular_multi_processing/master_script.py
@@ -199,15 +199,15 @@ def restart_ec_multiple_instances():
 #    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d_4.py")
 
 
-##### THIS IS THE MODULE2 SCRIPT with stub logic in the threaded_install
-#def install_tomcat_on_instances():
-#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d_5.py")
-
-
-
 #### THIS IS THE MODULE2 SCRIPT with stub logic in the threaded_install
 def install_tomcat_on_instances():
-    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d_6.py")
+    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d_5.py")
+
+
+
+##### THIS IS THE MODULE2 SCRIPT with stub logic in the threaded_install
+#def install_tomcat_on_instances():
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch7d_6.py")
 
 
 
