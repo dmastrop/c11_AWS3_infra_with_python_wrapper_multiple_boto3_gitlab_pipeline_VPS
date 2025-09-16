dd16f4fcb217cbf49543cd5ec3a1b9245edc3ca5
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-09-16 22:48:01 +0000

    added strace to the other 3 negative test cases (leave last one as is because it is an apt command)

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4b.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4b.py
@@ -3343,7 +3343,9 @@ def tomcat_worker(instance_info, security_group_ids, max_workers):
         # strace still no STDERR with above. Try this, writing directly to /dev/stderr
         #"strace -e write,execve -o /dev/stderr sudo bash -c 'echo test > /root/testfile'",
 
-        "strace -e write,execve -o /tmp/trace.log sudo bash -c 'echo test > /root/testfile'",
+        # apply strace with this methodology.  Write the logs to /tmp/trace.log  
+        # THIS IS WORKING with the added logic in install_tomcat to write the /tmp/trace.log to stderr
+        #"strace -e write,execve -o /tmp/trace.log sudo bash -c 'echo test > /root/testfile'",
 
 
 
@@ -3351,14 +3353,34 @@ def tomcat_worker(instance_info, security_group_ids, max_workers):
 
 
         # touch: cannot touch '/root/testfile': Permission denied. This is not throwing any STDOUT or STDERR and exit_code is 0
-        "sudo touch /root/testfile",
-        
+        #"sudo touch /root/testfile",
+
+        # apply strace to the command above
+        "strace -e write,execve -o /tmp/trace.log sudo touch /root/testfile",
+
+
+
+
         # bash: nonexistent_command: command not found
-        "bash -c \"nonexistent_command\"",
+        #"bash -c \"nonexistent_command\"",
+
+        # apply strace to the command above
+        "strace -e write,execve -o /tmp/trace.log sudo touch /root/testfile",
+
+
+
+
 
         # small script that exits with error and writes to STDERR
-        "bash -c \"echo -e '#!/bin/bash\\necho \\\"This is stderr\\\" >&2\\nexit 1' > /tmp/fail.sh && chmod +x /tmp/fail.sh && sudo /tmp/fail.sh\"",
-        
+        #"bash -c \"echo -e '#!/bin/bash\\necho \\\"This is stderr\\\" >&2\\nexit 1' > /tmp/fail.sh && chmod +x /tmp/fail.sh && sudo /tmp/fail.sh\"",
+
+        # apply strace to the command above
+        "strace -e write,execve -o /tmp/trace.log bash -c \"echo -e '#!/bin/bash\\necho \\\"This is stderr\\\" >&2\\nexit 1' > /tmp/fail.sh && chmod +x /tmp/fail.sh && sudo /tmp/fail.sh\"",
+
+
+
+
+
         # E: Unable to locate package tomcat99 (BLOCK2 failure heuristic check install_failed)
         "sudo apt install tomcat99",
 
