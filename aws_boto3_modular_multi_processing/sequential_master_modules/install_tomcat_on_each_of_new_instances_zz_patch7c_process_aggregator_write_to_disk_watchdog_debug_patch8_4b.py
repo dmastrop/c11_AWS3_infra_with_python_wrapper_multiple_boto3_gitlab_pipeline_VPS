178060b97da57c30725722d1cbcba76385916101
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-09-16 00:26:29 +0000

    add a pipe in the strace log to STDERR so the failure logic can take it from there. WE are now getting exit_code 1

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4b.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4b.py
@@ -3311,9 +3311,12 @@ def tomcat_worker(instance_info, security_group_ids, max_workers):
         # force a shell level failure. Not seeing STDOUT, STDERR and exit code is 0 verified with raw print of exit_code
         #"sudo bash -c 'echo test > /root/testfile'",
 
-        # test out the strace on the echo test above
-        "strace -e write,execve -o /tmp/trace.log sudo bash -c 'echo test > /root/testfile' && cat /tmp/trace.log",
-
+        # test out the strace on the echo test above. We are now getting exit_code=1 which is good but no logging
+        #"strace -e write,execve -o /tmp/trace.log sudo bash -c 'echo test > /root/testfile' && cat /tmp/trace.log",
+ 
+        # strace same as above but pipe all the strace error (-1) log lines to STDERR. The rest of the logic will take 
+        # care of tagging the registry_entry status for this.
+        "strace -e write,execve -o /tmp/trace.log sudo bash -c 'echo test > /root/testfile'; grep -E ' = -1 ' /tmp/trace.log >&2"
 
 
 
