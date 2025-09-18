413001b40708e880823d40b153c1d21b02348fa5
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-09-18 00:38:16 +0000

    file 8_4c1.py for the strace wrapper function and pre-processsing

--- aws_boto3_modular_multi_processing/master_script.py
+++ aws_boto3_modular_multi_processing/master_script.py
@@ -235,11 +235,13 @@ def restart_ec_multiple_instances():
 #    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4b.py")
 
 
+##### THIS IS THE MODULE2 SCRIPT with testing on 512 node and APT Whitelist additions
+#def install_tomcat_on_instances():
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4c.py")
+
 #### THIS IS THE MODULE2 SCRIPT with wrapper code for command agnotic approach to support bash and bash-like commands, etc.
 def install_tomcat_on_instances():
-    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4c.py")
-
-
+    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4c1.py")
 
 
 
