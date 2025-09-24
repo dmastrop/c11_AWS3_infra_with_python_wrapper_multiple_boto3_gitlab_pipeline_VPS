21317d967185b3107f00bb629f5268bfde4ae3e0
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-09-24 23:15:31 +0000

    regression test case 3

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4d.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_4d.py
@@ -3755,13 +3755,13 @@ def tomcat_worker(instance_info, security_group_ids, max_workers):
 
         # Test Case 2: Exit 0 + no stderr (install_success)
         # Test three in a row and ensure that trace.log is unique for each of them.
-        "bash -c 'echo \"hello world\" > /tmp/testfile'",
+        #"bash -c 'echo \"hello world\" > /tmp/testfile'",
         #"bash -c 'echo \"hello world\" > /tmp/testfile'",
         #"bash -c 'echo \"hello world\" > /tmp/testfile'",
         
 
         # Test Case 3: Nonzero exit + stderr from sudo
-        #"sudo touch /root/testfile",
+        "sudo touch /root/testfile",
 
         # Test Case 4: Nonexistent command (exit 127)
         #"bash -c \"nonexistent_command\"",
