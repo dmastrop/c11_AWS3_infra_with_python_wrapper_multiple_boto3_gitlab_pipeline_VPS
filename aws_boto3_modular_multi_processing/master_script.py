ef3c5c5a0ecae4fc54fe79675791512a349f3adb
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-10-03 20:56:23 +0000

    create new 8_5c.py file for troubleshooting the SG tomcat_worker issue

--- aws_boto3_modular_multi_processing/master_script.py
+++ aws_boto3_modular_multi_processing/master_script.py
@@ -255,12 +255,14 @@ def restart_ec_multiple_instances():
 #    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5.py")
 
 
-#### THIS IS THE MODULE2 SCRIPT with the refactor for the adaptive watchdog timeout refactoring continued.
+##### THIS IS THE MODULE2 SCRIPT with the refactor for the adaptive watchdog timeout refactoring continued.
+#def install_tomcat_on_instances():
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5b.py")
+
+
+#### THIS IS THE MODULE2 SCRIPT with code to determine why SG blocks are not executing in tomcat_worker
 def install_tomcat_on_instances():
-    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5b.py")
-
-
-
+    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5c.py")
 
 
 ## modules 3-11:
