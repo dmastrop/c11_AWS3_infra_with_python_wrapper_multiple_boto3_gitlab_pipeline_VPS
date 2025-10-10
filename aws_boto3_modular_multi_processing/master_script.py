ebb09da8335ac999128e67965e9fcb332799d899
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-10-10 22:26:36 +0000

    update master script

--- aws_boto3_modular_multi_processing/master_script.py
+++ aws_boto3_modular_multi_processing/master_script.py
@@ -265,11 +265,15 @@ def restart_ec_multiple_instances():
 #    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5c.py")
 
 
-    #### THIS IS THE MODULE2 SCRIPT with code to for refactoring resurrection_monitor_patch8 RESMON_8
+###### THIS IS THE MODULE2 SCRIPT with code to for refactoring resurrection_monitor_patch8 RESMON_8
+#def install_tomcat_on_instances():
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_6.py")
+
+
+
+##### THIS IS THE MODULE2 SCRIPT with code to for refactoring resurrection_monitor_patch8 RESMON_8 and getting rid of res reg lock
 def install_tomcat_on_instances():
-    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_6.py")
-
-
+    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_6b.py")
 
 
 ## modules 3-11:
