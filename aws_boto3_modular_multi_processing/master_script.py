75b797f591cededa6a07e7a5d0000e7756acf4eb
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-10-10 01:59:15 +0000

    forgot to update master script.py. Restart the 512 test

--- aws_boto3_modular_multi_processing/master_script.py
+++ aws_boto3_modular_multi_processing/master_script.py
@@ -260,9 +260,16 @@ def restart_ec_multiple_instances():
 #    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5b.py")
 
 
-#### THIS IS THE MODULE2 SCRIPT with code to determine why SG blocks are not executing in tomcat_worker
+##### THIS IS THE MODULE2 SCRIPT with code to determine why SG blocks are not executing in tomcat_worker
+#def install_tomcat_on_instances():
+#    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5c.py")
+
+
+    #### THIS IS THE MODULE2 SCRIPT with code to for refactoring resurrection_monitor_patch8 RESMON_8
 def install_tomcat_on_instances():
-    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_5c.py")
+    run_module("/aws_EC2/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_6.py")
+
+
 
 
 ## modules 3-11:
