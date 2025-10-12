6b4870343ddfcf4078a1a0f46d3c62aa3d35c2fb
Dave Mastropolo <dave.mastropolo@gmail.com>
2025-10-12 23:55:16 +0000

    switch to 512 node testing

--- aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_6c.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/install_tomcat_on_each_of_new_instances_zz_patch7c_process_aggregator_write_to_disk_watchdog_debug_patch8_6c.py
@@ -6329,9 +6329,9 @@ def main():
 
 
     ### Configurable parameters
-    chunk_size = 2 # Number of IPs per process; chunk_size should be less than or equal to max_workers, otherwise inefficiency results.
-    max_workers = 2 # Threads per process
-    desired_count = 6  # Max concurrent processes (NOT threads) for iniital batch.
+    chunk_size = 1 # Number of IPs per process; chunk_size should be less than or equal to max_workers, otherwise inefficiency results.
+    max_workers = 1 # Threads per process
+    desired_count = 487  # Max concurrent processes (NOT threads) for iniital batch.
     #### For the 16 node test chunk_size of 2, max_workers of 2, and desired_count of 6 so that 2 processes are pooled for the
     #### last 4 of 16 nodes
     #### For the 512 test, it is one thread per process, so: chunk_size of 1, max_workers of 1, desired_count of 487 so that 
