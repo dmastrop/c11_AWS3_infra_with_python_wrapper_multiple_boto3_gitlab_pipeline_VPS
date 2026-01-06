ec22817c36ab22203ffdb2ea730980c3f149f383
Dave Mastropolo <dave.mastropolo@gmail.com>
2026-01-06 01:30:15 +0000

    the SG rule state machine is working well. Add 7000 and 7001 and remove 6001.

--- aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_97.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_97.py
@@ -287,8 +287,10 @@ SG_RULES = [
     {"protocol": "tcp", "port": 5557, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 5558, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 6000, "cidr": "0.0.0.0/0"},
-    {"protocol": "tcp", "port": 6001, "cidr": "0.0.0.0/0"},
+    #{"protocol": "tcp", "port": 6001, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 6002, "cidr": "0.0.0.0/0"},
+    {"protocol": "tcp", "port": 7000, "cidr": "0.0.0.0/0"},
+    {"protocol": "tcp", "port": 7001, "cidr": "0.0.0.0/0"},
 ]
 
 
