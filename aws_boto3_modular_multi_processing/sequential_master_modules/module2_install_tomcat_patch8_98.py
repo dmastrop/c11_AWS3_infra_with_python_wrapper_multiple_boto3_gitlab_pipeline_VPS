152edde576528377ad52b09707ee03bbf6e03a21
Dave Mastropolo <dave.mastropolo@gmail.com>
2026-01-08 03:02:10 +0000

    revoke rule only for ssh timeoout issue

--- aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_98.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_98.py
@@ -295,7 +295,7 @@ SG_RULES = [
     #{"protocol": "tcp", "port": 7003, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 7004, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 7005, "cidr": "0.0.0.0/0"},
-    {"protocol": "tcp", "port": 7006, "cidr": "0.0.0.0/0"},
+    #{"protocol": "tcp", "port": 7006, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 7007, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 7008, "cidr": "0.0.0.0/0"},
 ]
