ec3994aab694fccd8e957fa47b8ce96125303ef3
Dave Mastropolo <dave.mastropolo@gmail.com>
2026-01-12 20:55:58 +0000

    remove port 9008 to prep for paramiko debugging in module2

--- aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_99.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_99.py
@@ -313,7 +313,7 @@ SG_RULES = [
     {"protocol": "tcp", "port": 9005, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 9006, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 9007, "cidr": "0.0.0.0/0"},
-    {"protocol": "tcp", "port": 9008, "cidr": "0.0.0.0/0"},
+    #{"protocol": "tcp", "port": 9008, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 9009, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 9010, "cidr": "0.0.0.0/0"},
 
