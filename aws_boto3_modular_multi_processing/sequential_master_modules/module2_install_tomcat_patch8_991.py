1b95d4de5ec57c337794f7f2eb94ab2aa26c2408
Dave Mastropolo <dave.mastropolo@gmail.com>
2026-01-13 00:54:46 +0000

    remove rule 9007 and retest with the port variable fix in place

--- aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_991.py
+++ aws_boto3_modular_multi_processing/sequential_master_modules/module2_install_tomcat_patch8_991.py
@@ -315,7 +315,7 @@ SG_RULES = [
     {"protocol": "tcp", "port": 9004, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 9005, "cidr": "0.0.0.0/0"},
     {"protocol": "tcp", "port": 9006, "cidr": "0.0.0.0/0"},
-    {"protocol": "tcp", "port": 9007, "cidr": "0.0.0.0/0"},
+    #{"protocol": "tcp", "port": 9007, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 9008, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 9009, "cidr": "0.0.0.0/0"},
     #{"protocol": "tcp", "port": 9010, "cidr": "0.0.0.0/0"},
